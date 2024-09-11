470f5296984d2b4114e51110a891db8bc04438aa
unknown <Rando.Tarkmeel@tktk.ee git config --global user.name>
2024-09-11 15:22:48 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Enter your name:");
+String userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+Console.WriteLine($"Hello, {userName}!");
